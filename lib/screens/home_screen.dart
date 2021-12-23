e2b2c488d4bec3327a2a1ebd9a72308cc3b27d25
Last-dreamer <asim0000.ak@gmail.com>
2021-12-23 05:30:55 +0500

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,37 @@
+import 'package:ecom_admin/screens/product_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My ecom"),
+        backgroundColor: Colors.black,
+      ),
+      body: SizedBox(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: double.infinity,
+              height: 150,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: InkWell(
+                onTap: (){
+                  Get.to(() => const ProductScreen());
+                },
+                child: const Card(
+                  child: Text("Go To Products"),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/product_screen.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+class ProductScreen extends StatelessWidget {
+  const ProductScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Products"),
+        backgroundColor: Colors.black,
+      ),
+      body: Container(),
+    );
+  }
+}
